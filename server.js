4461d7873db59db17770b5602733454750b06e4a
Patrick Jeremiah <patrickjerry59@yahoo.com>
2022-03-31 15:18:59 +0100

    created cart, register and login page.

--- server.js
+++ server.js
@@ -11,15 +11,29 @@ const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true}))
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
 
 app.use(expressLayouts);
 app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
+
+app.get('/cart', (req, res) => {
+    res.render('customers/cart')
+});
+
+app.get('/login', (req, res) => {
+    res.render('auth/login')
+});
+
+app.get('/register', (req, res) => {
+    res.render('auth/register')
+});
+
 
 
 app.listen(port, () => {
